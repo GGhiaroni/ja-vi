51fbee04250fa06d07447ce90d4f00a1adc4f4dd
GGhiaroni <gghiaronitiziano@gmail.com>
2025-11-03 15:47:18 -0300

    instalando o react router dom e ajustando o app.tsx com o roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="min-h-screen flex justify-center">
-      <Home />
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="minhalista" element={<MinhaLista />} />
+        </Routes>
+      </main>
     </div>
   );
 }
