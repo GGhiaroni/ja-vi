59d5b53074134632ed1deb2724f80bca9d56165f
GGhiaroni <gghiaronitiziano@gmail.com>
2025-11-12 16:08:58 -0300

    passando o id do item dentro do Link no componente MediaCard

--- src/pages/DetalhesMedia.tsx
+++ src/pages/DetalhesMedia.tsx
@@ -1,5 +1,20 @@
-const DetalhesMedia = () => {
-  return <div>DetalhesMedia</div>;
+import React from "react";
+import { useParams } from "react-router-dom";
+import ErrorMessage from "../components/ErrorMessage";
+import { useMediaStore } from "../store/useMediaStore";
+
+const DetalhesMedia: React.FC = () => {
+  const { media_type, id } = useParams<{ media_type: string; id: string }>();
+
+  if (!media_type || !id) {
+    return <ErrorMessage message="URL inválida." />;
+  }
+
+  const buscaIdNoCache = useMediaStore((state) =>
+    state.mediaList.find(
+      (item) => item.id === Number(id) && item.media_type == media_type
+    )
+  );
 };
 
 export default DetalhesMedia;
