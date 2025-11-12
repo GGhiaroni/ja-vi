0b4698721e0fdd982a58c2aba2dcddaab3f9b5c2
GGhiaroni <gghiaronitiziano@gmail.com>
2025-11-12 16:05:35 -0300

    adicionando o id como parametro na rota que leva para os detalhes do filme ou série

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="minhalista" element={<MinhaLista />} />
           <Route
-            path="/media/:media_type/:titulo"
+            path="/media/:media_type/:id/:titulo"
             element={<DetalhesMedia />}
           />
         </Routes>
