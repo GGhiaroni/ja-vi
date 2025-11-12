7d6e0b873c6bee92eaf4061b081156104038bb9a
GGhiaroni <gghiaronitiziano@gmail.com>
2025-11-12 15:59:50 -0300

    criando a funcao que faz o fetch na api do tmdb para buscar um filme ou serie pelo id

--- src/api/config.ts
+++ src/api/config.ts
@@ -4,3 +4,7 @@ const BASE_URL = "https://api.themoviedb.org/3";
 export const TRENDING_URL = `${BASE_URL}/trending/all/day?api_key=${API_KEY}&language=pt-BR`;
 
 export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+
+export const getMediaDetailsURL = (mediaType: string, id: string): string => {
+  return `${BASE_URL}/${mediaType}/${id}?api_key=${API_KEY}&language=pt-BR`;
+};
