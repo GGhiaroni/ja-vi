ce9b2362fe12caf2a05ab159bb2edcbbea0970a1
GGhiaroni <gghiaronitiziano@gmail.com>
2025-11-13 16:29:48 -0300

    alterando a cor do loading spinner para as cores do projeto

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const LoadingSpinner: React.FC = () => (
   <div className="flex justify-center items-center h-48">
     <div
-      className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"
+      className="animate-spin rounded-full h-12 w-12 border-b-2 border-(--color-ja-vi-secondary)"
       aria-label="Carregando conteúdo"
     />
-    <p className="ml-4 text-gray-500">Carregando...</p>
+    <p className="ml-4 text-(--color-ja-vi-secondary)">Carregando...</p>
   </div>
 );
 
